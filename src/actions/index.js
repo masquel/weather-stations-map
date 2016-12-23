49c1847688ddff967b1bf7e1dc10528b7ebb22a3
Alexey <grimzest@gmail.com>
2016-12-24 00:14:39 +0300

    update query string in js

--- src/actions/index.js
+++ src/actions/index.js
@@ -99,84 +99,46 @@ const setStation = (station) => {
 
 export const fetchStation = (id,name,{year, month}) => {
 	/*
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-  PREFIX dbr: <http://dbpedia.org/resource/>
-
-  PREFIX dbo: <http://dbpedia.org/ontology/>
-
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-  PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-
-  PREFIX mt: <http://example.org/meteo_ru_data/>
-
-  PREFIX mto: <http://example.org/meteo_ru_data/ontology/>
-
-  PREFIX mtr: <http://example.org/meteo_ru_data/resource/>
-
-  PREFIX schema: <http://schema.org/>
-
-  PREFIX ispra: <http://dati.isprambiente.it/ontology/core#>
-
-  PREFIX dcterms: <http://purl.org/dc/terms/>
-
-
-
-  SELECT ?measure ?date ?stat_num ?precipitation ?cr_flag ?qr_flag ?tflag ?tmax ?qtmax_flag ?tmean ?qtmean_flag ?tmin ?qtmin
-
-  WHERE
-
-  {
-
-  ?measure a mto:w_measure;
-
-  mto:datem ?date;
-
-  mto:st_measure ?station;
-
-  mto:precip ?precipitation;
-
-  mto:cr ?cr_flag;
-
-  mto:qr ?qr_flag;
-
-  mto:tflag ?tflag;
-
-  mto:tmax ?tmax;
-
-  mto:qtmax ?qtmax_flag;
-
-  mto:tmean ?tmean;
-
-  mto:qtmean ?qtmean_flag;
-
-  mto:tmin ?tmin;
-
-  mto:qtmin ?qtmin_flag.
-
-
-
-  ?station a ispra:MeasureStation;
-
-  dcterms:identifier 21908.
-
-
-  	BIND(year(xsd:date(?date)) as ?date_year).
-  			BIND(month(xsd:date(?date)) as ?date_month).
-  			FILTER (?date_year=2015 && ?date_month=2)
-
-
-  }
-
-  ORDER BY DESC(?date)
-
-  LIMIT 31
+		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+		PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+		PREFIX dbr: <http://dbpedia.org/resource/>
+		PREFIX dbo: <http://dbpedia.org/ontology/>
+		PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+		PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+		PREFIX mt: <http://example.org/meteo_ru_data/>
+		PREFIX mto: <http://example.org/meteo_ru_data/ontology/>
+		PREFIX mtr: <http://example.org/meteo_ru_data/resource/>
+		PREFIX schema: <http://schema.org/>
+		PREFIX ispra: <http://dati.isprambiente.it/ontology/core#>
+		PREFIX dcterms: <http://purl.org/dc/terms/>
+		SELECT ?measure ?date ?stat_num ?precipitation ?cr_flag ?qr_flag ?tflag ?tmax ?qtmax_flag ?tmean ?qtmean_flag ?tmin ?qtmin
+		WHERE
+		{
+			?measure a mto:w_measure;
+			mto:datem ?date;
+			mto:st_measure ?station;
+			mto:precip ?precipitation;
+			mto:cr ?cr_flag;
+			mto:qr ?qr_flag;
+			mto:tflag ?tflag;
+			mto:tmax ?tmax;
+			mto:qtmax ?qtmax_flag;
+			mto:tmean ?tmean;
+			mto:qtmean ?qtmean_flag;
+			mto:tmin ?tmin;
+			mto:qtmin ?qtmin_flag.
+			?station a ispra:MeasureStation;
+			dcterms:identifier 21908.
+			BIND(year(xsd:date(?date)) as ?date_year).
+			BIND(month(xsd:date(?date)) as ?date_month).
+			FILTER (?date_year=2015 && ?date_month=2)
+		}
+		ORDER BY DESC(?date)
+		LIMIT 31
 	*/
 	console.log(year,month);
-	const query = `PREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0APREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0APREFIX%20dbr%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0APREFIX%20dbo%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0APREFIX%20xsd%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0APREFIX%20geo%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0APREFIX%20mt%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2F%3E%0APREFIX%20mto%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2Fontology%2F%3E%0APREFIX%20mtr%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2Fresource%2F%3E%0ASELECT%20%3Fmeasure%20%3Fdate%20%3Fstat_num%20%3Fprecipitation%20%3Fcr_flag%20%3Fqr_flag%20%3Ftflag%20%3Ftmax%20%3Fqtmax_flag%20%3Ftmean%20%3Fqtmean_flag%20%3Ftmin%20%3Fqtmin%0AWHERE%20%0A%7B%20%0A%3Fmeasure%20a%20mto%3Aw_measure%3B%0Amto%3Adatem%20%3Fdate%3B%0Amto%3Ast_measure%20%3Fstation%3B%0Amto%3Aprecip%20%3Fprecipitation%3B%0Amto%3Acr%20%3Fcr_flag%3B%0Amto%3Aqr%20%3Fqr_flag%3B%0Amto%3Atflag%20%3Ftflag%3B%0Amto%3Atmax%20%3Ftmax%3B%0Amto%3Aqtmax%20%3Fqtmax_flag%3B%0Amto%3Atmean%20%3Ftmean%3B%0Amto%3Aqtmean%20%3Fqtmean_flag%3B%0Amto%3Atmin%20%3Ftmin%3B%0Amto%3Aqtmin%20%3Fqtmin_flag.%0A%3Fstation%20a%20mto%3Aw_st%20%3B%0Amto%3Ast_num%20${id}.%0A%20%20BIND(year(xsd%3Adate(%3Fdate))%20as%20%3Fdate_year).%0A%20%20BIND(month(xsd%3Adate(%3Fdate))%20as%20%3Fdate_month).%0A%20%20FILTER%20(%3Fdate_year%3D${year}%20%26%26%20%3Fdate_month%3D${month})%0A%7D%0AORDER%20BY%20DESC(%3Fdate)%0ALIMIT%2031`;
+	//const query = `PREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0APREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0APREFIX%20dbr%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0APREFIX%20dbo%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0APREFIX%20xsd%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0APREFIX%20geo%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0APREFIX%20mt%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2F%3E%0APREFIX%20mto%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2Fontology%2F%3E%0APREFIX%20mtr%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2Fresource%2F%3E%0ASELECT%20%3Fmeasure%20%3Fdate%20%3Fstat_num%20%3Fprecipitation%20%3Fcr_flag%20%3Fqr_flag%20%3Ftflag%20%3Ftmax%20%3Fqtmax_flag%20%3Ftmean%20%3Fqtmean_flag%20%3Ftmin%20%3Fqtmin%0AWHERE%20%0A%7B%20%0A%3Fmeasure%20a%20mto%3Aw_measure%3B%0Amto%3Adatem%20%3Fdate%3B%0Amto%3Ast_measure%20%3Fstation%3B%0Amto%3Aprecip%20%3Fprecipitation%3B%0Amto%3Acr%20%3Fcr_flag%3B%0Amto%3Aqr%20%3Fqr_flag%3B%0Amto%3Atflag%20%3Ftflag%3B%0Amto%3Atmax%20%3Ftmax%3B%0Amto%3Aqtmax%20%3Fqtmax_flag%3B%0Amto%3Atmean%20%3Ftmean%3B%0Amto%3Aqtmean%20%3Fqtmean_flag%3B%0Amto%3Atmin%20%3Ftmin%3B%0Amto%3Aqtmin%20%3Fqtmin_flag.%0A%3Fstation%20a%20mto%3Aw_st%20%3B%0Amto%3Ast_num%20${id}.%0A%20%20BIND(year(xsd%3Adate(%3Fdate))%20as%20%3Fdate_year).%0A%20%20BIND(month(xsd%3Adate(%3Fdate))%20as%20%3Fdate_month).%0A%20%20FILTER%20(%3Fdate_year%3D${year}%20%26%26%20%3Fdate_month%3D${month})%0A%7D%0AORDER%20BY%20DESC(%3Fdate)%0ALIMIT%2031`;
+	const query = `PREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0A%09%09PREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0A%09%09PREFIX%20dbr%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0A%09%09PREFIX%20dbo%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0A%09%09PREFIX%20xsd%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0A%09%09PREFIX%20geo%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0A%09%09PREFIX%20mt%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2F%3E%0A%09%09PREFIX%20mto%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2Fontology%2F%3E%0A%09%09PREFIX%20mtr%3A%20%3Chttp%3A%2F%2Fexample.org%2Fmeteo_ru_data%2Fresource%2F%3E%0A%09%09PREFIX%20schema%3A%20%3Chttp%3A%2F%2Fschema.org%2F%3E%0A%09%09PREFIX%20ispra%3A%20%3Chttp%3A%2F%2Fdati.isprambiente.it%2Fontology%2Fcore%23%3E%0A%09%09PREFIX%20dcterms%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0A%09%09SELECT%20%3Fmeasure%20%3Fdate%20%3Fstat_num%20%3Fprecipitation%20%3Fcr_flag%20%3Fqr_flag%20%3Ftflag%20%3Ftmax%20%3Fqtmax_flag%20%3Ftmean%20%3Fqtmean_flag%20%3Ftmin%20%3Fqtmin%0A%09%09WHERE%0A%09%09%7B%0A%09%09%09%3Fmeasure%20a%20mto%3Aw_measure%3B%0A%09%09%09mto%3Adatem%20%3Fdate%3B%0A%09%09%09mto%3Ast_measure%20%3Fstation%3B%0A%09%09%09mto%3Aprecip%20%3Fprecipitation%3B%0A%09%09%09mto%3Acr%20%3Fcr_flag%3B%0A%09%09%09mto%3Aqr%20%3Fqr_flag%3B%0A%09%09%09mto%3Atflag%20%3Ftflag%3B%0A%09%09%09mto%3Atmax%20%3Ftmax%3B%0A%09%09%09mto%3Aqtmax%20%3Fqtmax_flag%3B%0A%09%09%09mto%3Atmean%20%3Ftmean%3B%0A%09%09%09mto%3Aqtmean%20%3Fqtmean_flag%3B%0A%09%09%09mto%3Atmin%20%3Ftmin%3B%0A%09%09%09mto%3Aqtmin%20%3Fqtmin_flag.%0A%09%09%09%3Fstation%20a%20ispra%3AMeasureStation%3B%0A%09%09%09dcterms%3Aidentifier%20${id}.%0A%09%09%09BIND(year(xsd%3Adate(%3Fdate))%20as%20%3Fdate_year).%0A%09%09%09BIND(month(xsd%3Adate(%3Fdate))%20as%20%3Fdate_month).%0A%09%09%09FILTER%20(%3Fdate_year%3D${year}%20%26%26%20%3Fdate_month%3D${month})%0A%09%09%7D%0A%09%09ORDER%20BY%20DESC(%3Fdate)%0A%09%09LIMIT%2031`;
 	return dispatch => {
 		dispatch(setActiveMonth({year: year, month: month}));
 		dispatch(loadStation(id,name))
